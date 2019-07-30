efccfcd86a5349cb001347625c193f289c054202
Darek K <darius.kloda@gmail.com>
2019-07-30 16:55:09 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# DummyStore
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import "./styles.css";
+
+function App() {
+	return <div className="App" />;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
